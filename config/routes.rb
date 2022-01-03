c2815fa71b6902762dfb050c5d396f31e5d50dd0
samwinslow <sammywinslow@gmail.com>
2022-01-03 14:20:04 -0500

    add signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
 
   resources :posts
   root "posts#index"
@@ -6,5 +9,9 @@ Rails.application.routes.draw do
   resources :registrations
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
