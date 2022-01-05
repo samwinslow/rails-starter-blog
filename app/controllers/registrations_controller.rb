42818df36aa3f0ed67efeae6231074aeebde5eec
samwinslow <sammywinslow@gmail.com>
2022-01-05 15:07:39 -0500

    fix typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.after_save
+    if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
       else
